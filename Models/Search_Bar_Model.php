ca75a5b0117cff7c77fc43fbe2ca1716ba09bfaf
Ryan Lelliott <lelliott.ryan@gmail.com>
2021-01-13 12:43:29 +0000

    The view, controller and model for the search bar.
    View: Complete.
    "Fully Implemented."
    
    Controller: Incomplete.
    "Fully Implemented."
    
    Model: Incomplete.
    "1) Requires database connectors.
     2) Requires SQL statements, these I am working on."
    
    Signed-off-by: Ryan Lelliott <lelliott.ryan@gmail.com>

--- Models/Search_Bar_Model.php
+++ Models/Search_Bar_Model.php
@@ -1,5 +1,96 @@
 <?php
 
-// Model Here
+// Global Variable Calls
+global $searchResolvedStatus;
+global $searchRequest;
+global $searchLine;
+
+// The switch statement initially checks for which radio button filter has been selected,
+// then it decided if the 'resolved' status is true or false,
+// this determines the SQL statement that is chosen.
+switch($searchRequest)
+{
+
+    // Search by Customer
+    case "1":
+
+        if ($searchResolvedStatus != "1")
+        {
+
+            $sqlQuery = '';
+
+            break;
+
+        }
+        else
+        {
+
+            $sqlQuery = '';
+
+            break;
+
+        }
+
+    // Search by Problem
+    case "2":
+
+        if ($searchResolvedStatus != "1")
+        {
+
+            $sqlQuery = '';
+
+            break;
+
+        }
+        else
+        {
+
+            $sqlQuery = '';
+
+            break;
+
+        }
+
+    // Search by Category
+    case "3":
+
+        if ($searchResolvedStatus != "1")
+        {
+
+            $sqlQuery = '';
+
+            break;
+
+        }
+        else
+        {
+
+            $sqlQuery = '';
+
+            break;
+
+        }
+
+    // Search by Staff
+    case "4":
+
+        if ($searchResolvedStatus != "1")
+        {
+
+            $sqlQuery = '';
+
+            break;
+
+        }
+        else
+        {
+
+            $sqlQuery = '';
+
+            break;
+
+        }
+
+}
 
 ?>
