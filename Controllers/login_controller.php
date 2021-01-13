800af4460d661e5af555001c2e32b63acf19dc5b
Naxane <conor.lawton1@gmailcom>
2021-01-13 15:35:10 +0000

    Abstracted controller class

--- Controllers/login_controller.php
+++ Controllers/login_controller.php
@@ -1,32 +1,36 @@
 <?php
-
-	require_once __DIR__ . "/../Models/DatabaseModel.php";
 	
-	class LoginController {
+	require_once __DIR__ . "/../Models/database_model.php";
+	
+	class LoginController
+	{
 		
 		private $_dbHandle;
 		
-		function __construct() {
+		function __construct()
+		{
 			$this->_dbHandle = DatabaseModel::getInstance();
 		}
 		
-		function view() {
-			require_once __DIR__."/../Views/login_view.phtml";
+		function view()
+		{
+			require_once __DIR__ . "/../Views/login_view.phtml";
 		}
-
-		static function login_user($_email, $_password): ?UserModel {
-
+		
+		static function login_user($_email, $_password): ?UserModel
+		{
+			
 			$db = DatabaseModel::getInstance();
 			$login = $db->getDBConnection()->prepare("SELECT id, name, email, password FROM staff WHERE email = ? LIMIT 1");
 			$login->bind_param("s", $_email);
 			$login->bind_result($id, $name, $email, $password);
 			$login->execute();
-
+			
 			$result = $login->fetch();
 			if ($result === null) {
 				$_SESSION["login-message"] = "Username or password is incorrect.";
 				return null;
-			} else if($result === false) {
+			} else if ($result === false) {
 				$_SESSION["login-message"] = "An unknown error has occurred.";
 				return null;
 			}
@@ -42,14 +46,15 @@
 			}
 		}
 		
-		static function logout_user() {
+		static function logout_user()
+		{
 			$_SESSION["logged-in"] = false;
 			$_SESSION["user"] = null;
 			
 			header("Location: /");
 		}
 	}
-
+	
 	session_start();
 	if (isset($_POST["submit"])) {
 		$email = $_POST["email"];
--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
 	
+	require_once __DIR__ . '/Controllers/index_controller.php';
+	require_once __DIR__ . "/Core/controller_base.php";
+	
 	$url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'])) : '/';
 	
 	// If the user token isn't set re-route to the login page,
 	// Since this is a staff only page we don't need to create a "home page" for anyone else.
 	
 	if (isset($_SESSION["user"])) {
+		
+		// Accessing the root page of the site, serve the index page.
 		if ($url[0] == '/') {
-			
-			require_once __DIR__ . '/Controllers/index_controller.php';
 			
 			$user = $_SESSION["user"];
 			
@@ -21,10 +24,17 @@
 			$request_action = isset($url[1]) ? $url[1] : '';
 			$request_parameters = array_slice($url, 2);
 			
-			$controller_path = __DIR__ . '/Controllers/' . $requested_controller . '_controller.php';
+			// Get the path to the controller file
+			$controller_path = __DIR__ . "/Controllers/" . $requested_controller . "_controller.php";
 			
+			// Check if the controller exists, otherwise serve a 404
 			if (file_exists($controller_path)) {
-			
+				
+				// Perform some magic
+				$controller_name = ucfirst($controller_path);
+				$controller = new $controller_name();
+				$controller->view();
+				die();
 			} else {
 				header('HTTP/1.1 404 Not Found');
 				require_once "404.php";
@@ -32,7 +42,7 @@
 			}
 		}
 	} else {
-		require_once __DIR__."/Controllers/login_controller.php";
+		require_once __DIR__ . "/Controllers/login_controller.php";
 		
 		$login_controller = new LoginController();
 		$login_controller->view();
