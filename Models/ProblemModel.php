cb47147ae3695d237120c2bebee7dfcb25d379e2
Naxane <conor.lawton1@gmailcom>
2021-01-15 11:22:12 +0000

    Cherry picked check_if_exists

--- Models/ProblemModel.php
+++ Models/ProblemModel.php
@@ -53,6 +53,21 @@
 			$add_problem->close();
 		}
 		
+		public static function check_if_exists($id) {
+			
+			$db = DatabaseModel::getInstance();
+			
+			$check_exists = $db->getDBConnection()->prepare("SELECT 1 FROM problems WHERE id = ?;");
+			$check_exists->bind_param("i", $id);
+			$check_exists->execute();
+			
+			$result = $check_exists->fetch();
+			
+			$check_exists->close();
+			
+			return $result;
+		}
+		
 		public function display()
 		{
 			$problem = $this;
