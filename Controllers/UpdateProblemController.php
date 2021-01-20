b678047b9af02e0a42a05fbec0c784ee4dc082f6
yesdupmail <liampudsey2@gmail.com>
2021-01-20 18:15:50 +0000

    searchbar view looks prettier now

--- Controllers/UpdateProblemController.php
+++ Controllers/UpdateProblemController.php
@@ -6,34 +6,21 @@
 	
 	class UpdateProblemController extends ControllerBase
 	{
-		private $user;
-		
-		function __construct($user)
+		function __construct()
 		{
-			$this->user = $user;
-			$this->view = new ViewBase("Update Problem", "/Views/UpdateProblemView.phtml");
+
+
+
+		    $this->view = new ViewBase("Update Problem", "/Views/UpdateProblemView.phtml");
+		    $this->view->ID = $_GET['problemID'];
+		    $this->view->problem = ProblemModel::get_by_id($this->view->ID);
+
+
 		}
 		
 		function get(): void
 		{
-			$this->view->view();
-
-            $exists = CustomerModel::check_if_exists($_GET['CustomerID']);
-            if ($exists === true)
-            {
-                $escaped_text = htmlspecialchars($_GET['description'], ENT_QUOTES | ENT_HTML5);
-                ProblemModel::update_problem($_GET['urgency'], $escaped_text, $_GET['categorisation_id'], $this->user->id, $_GET['CustomerID']);
-            }
-            elseif ($exists === false)
-            {
-                $_SESSION["update_problem_message"] = "Failed to update problem: Unknown error";
-            }
-            elseif ($exists === null)
-            {
-                $_SESSION["invalid_customer_id"] = true;
-            }
-
-            header("Location: /UpdateProblem");
+            $this->view->view();
 		}
 		
 		function post(): void
